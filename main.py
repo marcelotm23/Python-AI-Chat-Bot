d4a71218c6c5b03dcf9eece6c20872b11df6066c
Marcelo Torrejón Manso <marcelo.torrejn@gmail.com>
2020-06-11 13:40:26 +0200

    part 2

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ words = []
 labels = []
 docs_x = []
 docs_y = []
-#Stammer, take each word in our pattern and bring it down to the root word
+# Stammer, take each word in our pattern and bring it down to the root word
+# to reduce the vocabulary of our model and attempt to find the more general
+# meaning behind sentences.
 for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds = nltk.word_tokenize(pattern)# return a list of words
@@ -28,3 +30,38 @@ for intent in data['intents']:
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
+
+# Lower words
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+# Sort words
+words = sorted(list(set(words)))
+# Sort labels
+labels = sorted(labels)
+
+
+# Training, look if the root word of different words from patterns are exist
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+# Transform input to numpy
+training = numpy.array(training)
+output = numpy.array(output)
